43a2030c137245ff2d47f164fbc8959c091d0170
OhChangEun <dwc07109@gmail.com>
2025-08-27 23:53:00 +0900

    solve: 코딩테스트 문제 풀이 - 15일차

---
+++ 2025-08-27/오창은/[solved] 프로그래머스_순위_BFS.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+class Solution {
+    List<List<Integer>> winGraph = new ArrayList<>();
+    List<List<Integer>> loseGraph = new ArrayList<>();
+    
+    public int solution(int n, int[][] results) {
+        for (int i=0; i<=n; i++) {
+            winGraph.add(new ArrayList<>());
+            loseGraph.add(new ArrayList<>());
+        }
+
+        for (int[] result: results) {
+            int win = result[0];
+            int lose = result[1];
+            
+            winGraph.get(win).add(lose);
+            loseGraph.get(lose).add(win);
+        }
+
+        int answer = 0;
+        for (int i=1; i<=n; i++) {
+            int winCount = bfs(i, winGraph, n);
+            int loseCount = bfs(i, loseGraph, n);
+            
+            if (winCount + loseCount == n-1) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+    public int bfs(int start, List<List<Integer>> graph, int n) {
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+		visited[start] = true;
+       
+        int count = 0;
+        while (!queue.isEmpty()) {
+            int curr = queue.poll();
+            for (int next: graph.get(curr)) {
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.offer(next);
+                    count++;
+                }
+            }
+
+
+
+        }
+        return count;
+
+    }
+}
